494ec2fd72615961d0929f7de893bc94f207b0b7
DavitPH <davitph0@gmail.com>
2022-12-22 16:47:30 +0700

    method insert

--- EXE5_20210140026_KELOMPOK2/Program.cs
+++ EXE5_20210140026_KELOMPOK2/Program.cs
@@ -11,6 +11,27 @@ namespace CircularQueues_CSharp
             davit = -1;
             hartono = -1;
         }
-       
+        public void insert(int element)
+        {
+            if ((davit == 0 && hartono == max - 1) || (davit == hartono + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            if (davit == -1)
+            {
+                davit = 0;
+                hartono = 0;
+            }
+            else
+            {
+                if (hartono == max - 1)
+                    hartono = 0;
+                else
+                    hartono = hartono + 1;
+            }
+            queue_array[hartono] = element;
+        }
+   
     }
 }
